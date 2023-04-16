25c4815b0ba3c97edbc6a924f3b1569632fb7034
TenLun <87482603+TenLun@users.noreply.github.com>
2023-04-16 12:35:49 +0800

    Update k.js

--- k.js
+++ k.js
@@ -6,12 +6,11 @@ class myextension {
 				color1: '#ff8829',
 				color2: '#ff8829',
 				color3: '#ff8829',
-				'标签',
 				blocks: [
 					{opcode: 'myblock',
 					blockType: Scratch.BlockType.COMMAND,
 					text: "我的积木",
-					arguments:{ }
+					arguments:{}
 					}
 				]
 			};
@@ -19,5 +18,5 @@ class myextension {
 		myblock(args) {
 			window.open('https://vdse.bdstatic.com//192d9a98d782d9c74c96f09db9378d93.mp4','_blank');
 		}
-};
+}
 Scratch.extensions.register(new myextension());
